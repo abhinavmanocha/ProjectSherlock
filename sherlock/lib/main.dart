51d106fd7bdc7b51fa21ae7fa4ac2fbd0682462e
Abhinav Manocha <59585693+abhinavmanocha@users.noreply.github.com>
2021-11-28 21:20:32 -0500

    Created basic form with correct input layout

--- sherlock/lib/main.dart
+++ sherlock/lib/main.dart
@@ -1,32 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-@immutable
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
+    final appTitle = 'Flutter Form Demo';
     return MaterialApp(
-        title: 'Sherlock',
-        home: Scaffold(
-          body: Container(
-              padding: const EdgeInsets.fromLTRB(0, 32, 0, 0),
-              child: Column(
-                children: [
-// Trent Forensics logo image
-                  Image.asset(
-                    'images/forensics_logo.jpg',
-                    width: 435,
-                    height: 85,
-                    fit: BoxFit.fitWidth,
-                  ),
-                ], // children
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Create a corresponding State class. This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment
+            .center, //This is what aligns the information to the middle of the page
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter your team name',
+              labelText: 'Name',
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter a pattern ID',
+              labelText: 'Pattern ID',
+            ),
+          ),
+          TextFormField(
+              decoration: InputDecoration(labelText: 'Number of Stains'),
+              keyboardType: TextInputType.number),
+          new Container(
+              padding: const EdgeInsets.only(left: 200.0, top: 40.0),
+              child: new RaisedButton(
+                child: const Text('Submit'),
+                onPressed: null,
               )),
-        ));
+        ],
+      ),
+    );
   }
 }
